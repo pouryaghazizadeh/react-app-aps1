a759992fedeca8820d3b330ffc94467f6efed13a
pourya ghazizadeh <pourya.gh095@gmail.com>
2022-02-01 12:48:44 +0100

    setup app

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import Navbar from "./components/navbar/Navbar";
+import { ThemeProvider } from "@mui/private-theming";
+import theme from "./constants/theme";
+const  App=()=> {
+  return (
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <Navbar />
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+        <div>hgu</div>
+      </ThemeProvider>
+    </div>
+  );
+}
+
+export default App;
