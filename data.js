e359adc16a085810ec36b7ca5ff09d6ff3aaeab5
pourya ghazizadeh <pourya.gh095@gmail.com>
2022-02-02 12:03:49 +0100

    make functiln

---
+++ data.js
@@ -0,0 +1,21 @@
+const sendData = ()=>{
+    return {
+      logoWebsite: {
+        srcLogo: "../images/apsLogo.jpg ",
+        alt: "APSignals",
+      },
+      routes: [
+        {
+          nameRoute: "Home",
+          route: "/",
+        },
+        {
+          nameRoute: "AboutUs",
+          route: "/aboutUs",
+        },
+      ],
+    };
+
+}
+
+module.export = sendData;
