4c474d02cc2306b7c945a009d01e51abd4c71d14
pourya ghazizadeh <pourya.gh095@gmail.com>
2022-02-02 19:27:03 +0100

    d

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,30 +1,39 @@
+import MenuIcon from "@mui/icons-material/Menu";
 import { AppBar, Box, Toolbar } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import React from "react";
 import headerData from "../../constants/headerData.json";
-import MenuIcon from "@mui/icons-material/Menu";
 import Buttons from "../button/Button";
+import { useState } from "react";
 export const navbarStyle = makeStyles((theme) => {
-  console.log(theme.breakpoints);
   return {
-    header: {
+    containerNavbar: {
       [theme.breakpoints.up("xs")]: {
-        display: "flex",
-        justifyContent: "center",
-
-        height: "80px",
+        // height: "75px",
       },
-     
     },
-    firstNav: {},
+    firstNav: {
+      [theme.breakpoints.up("xs")]: {
+        height: "100%",
+        // background: "green",
+      },
+      [theme.breakpoints.up("sm")]: {
+        height: "0px",
+      },
+    },
     secondNav: {
+      borderTop: "1px solid",
       [theme.breakpoints.up("xs")]: {
         display: "none",
+        background:"green"
+
       },
       [theme.breakpoints.up("md")]: {
         display: "flex",
-        justifyContent: "center",
-      },
+        justifyContent:"center",
+        height: "30px"
+
+            },
     },
     containerIcon: {
       [theme.breakpoints.up("sm")]: {
@@ -35,31 +44,38 @@ export const navbarStyle = makeStyles((theme) => {
       height: "60px",
       width: "70px",
       // background: "blue",
-   
-     
     },
   };
 });
 const Navbar = () => {
   const useStyle = navbarStyle();
+  const [mood,SetMood] = useState(false)
   return (
     <>
-      <AppBar color="default" className={useStyle.header}>
-        <Toolbar component="nav">
+      <AppBar color="default" className={useStyle.containerNavbar}>
+        <Toolbar component="section" className={useStyle.firstNav}>
           <Box flexGrow={2}>
-
-          <img
-            src={headerData.logoWebsite.srcLogo}
-            alt={headerData.logoWebsite.alt}
-            className={useStyle.logo}
+            <img
+              src={headerData.logoWebsite.srcLogo}
+              alt={headerData.logoWebsite.alt}
+              className={useStyle.logo}
             />
-            </Box>
-          <Box component="section" className={useStyle.containerIcon} >
-            <MenuIcon fontSize="large" />
+          </Box>
+          <Box component="section" className={useStyle.containerIcon}>
+            <MenuIcon
+              fontSize="large"
+              onClick={(e) => {
+                SetMood(!mood);
+                console.log(mood);
+              }}
+            />
           </Box>
         </Toolbar>
-
-        {/* <Toolbar component="nav" className={useStyle.secondNav}>
+        <Toolbar
+          component="nav"
+          // className={useStyle.secondNav}
+          sx={{ display: { xs: "none", sm: "block" } }}
+        >
           {headerData.routes.map((value, i) => {
             const { nameRoute, route } = value;
             const dataBtn = {
@@ -72,7 +88,7 @@ const Navbar = () => {
 
             return <Buttons key={i} Info={dataBtn} />;
           })}
-        </Toolbar> */}
+        </Toolbar>
       </AppBar>
     </>
   );
---
+++ refrenceData/navbar/README.md
@@ -0,0 +1,3 @@
+# reference-data-aps
+
+## make a
