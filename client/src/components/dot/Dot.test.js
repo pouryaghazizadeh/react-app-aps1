3b7d07693c5ad1fce66ee1dc4dd7fe7bbf702d1d
pourya ghazizadeh <pourya.gh095@gmail.com>
2022-02-08 12:00:01 +0100

    testing

--- client/src/components/dot/Dot.test.js
+++ client/src/components/dot/Dot.test.js
@@ -1,18 +1,17 @@
-import { render } from "@testing-library/react";
-import { makeStyles } from "@mui/styles";
+
 import Dot from "./Dot"
 import { ThemeProvider } from "@mui/private-theming";
 import theme from "../../constants/theme";
 import dataSliders from "../../constants/slider.json"
-
+import { shallow } from "enzyme";
 
  const dataSlider = {
    dataSliders,
  };
 test("render Dot",()=>{
-    render(
-      <ThemeProvider theme={theme}>
-        <Dot Info={dataSlider} />
-      </ThemeProvider>
-    );
+     shallow(
+       <ThemeProvider theme={theme}>
+         <Dot Info={dataSlider} />
+       </ThemeProvider>
+     );
 })
--- client/src/components/slider/Slider.test.js
+++ client/src/components/slider/Slider.test.js
@@ -1,7 +1,16 @@
-import { render } from "@testing-library/react";
+
 import Slider from "./Slider";
+import { shallow } from "enzyme";
+import dataSlider from "../../constants/slider.json"
+import { ThemeProvider } from "@mui/private-theming";
+
+import theme from "../../constants/theme";
+
 
 test('render slider', () => {
-    render(<Slider/>)
-  ;
+   shallow(
+     <ThemeProvider theme={theme}>
+       <Slider dataSlider={dataSlider} />
+     </ThemeProvider>
+   );
 });
